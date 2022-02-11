afbe2709097e4d1ae8e73f570fb46b7a103c0811
Rodney van den Velden <rodneymichael2002@gmail.com>
2022-02-11 15:30:47 +0100

    Feat: relative volume

--- commands/music/volume.js
+++ commands/music/volume.js
@@ -8,16 +8,16 @@ module.exports = {
 
     if (!queue || !queue.playing) return message.channel.send('There is no music currently playing.');
 
-    const vol = parseInt(args[0]);
+    if (!args) return message.channel.send(`Current volume: **${queue.volume}**\n**To change the volume, Type a number between \`1\` and \`${maxVol}\`.`);
 
-    if (!vol) return message.channel.send(`Current volume: **${queue.volume}**\n**To change the volume, Type a number between \`1\` and \`${maxVol}\`.`);
-
-    if (queue.volume === vol) return;
-
-    if (vol < 0 || vol > maxVol) return message.channel.send(`To change the volume, Type a number between \`1\` and \`${maxVol}\``);
-
-    const success = queue.setVolume(vol);
-
-    return message.channel.send(success ? `Volume changed: **${vol}**/**${maxVol}**` : 'Something went wrong.');
+    if (args[0].startsWith('+') || args[0].startsWith('-')) {
+      const relVol = parseInt(args[0].substr(1));
+      const success = queue.setVolume(queue.volume + toAdd);
+      return message.channel.send(success ? `Volume changed: **${vol}**/**${maxVol}**` : 'Something went wrong.');
+    } else {
+      const vol = parseInt(args[0]);
+      const success = queue.setVolume(vol);
+      return message.channel.send(success ? `Volume changed: **${vol}**/**${maxVol}**` : 'Something went wrong.');
+    }
   }
 }
