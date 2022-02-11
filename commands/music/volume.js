f982b99d759cf2461b69e1d1e7e26550f015d988
Rodney van den Velden <rodneymichael2002@gmail.com>
2022-02-11 15:33:19 +0100

    x

--- commands/music/volume.js
+++ commands/music/volume.js
@@ -12,7 +12,7 @@ module.exports = {
 
     if (args[0].startsWith('+') || args[0].startsWith('-')) {
       const relVol = parseInt(args[0].substr(1));
-      const success = queue.setVolume(queue.volume + reVol);
+      const success = queue.setVolume(queue.volume + relVol);
       return message.channel.send(success ? `Volume changed: **${vol}**/**${maxVol}**` : 'Something went wrong.');
     } else {
       const vol = parseInt(args[0]);
